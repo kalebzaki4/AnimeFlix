434e7477190565ca4c9740b94adbfc455074fb06
Kaleb Zufanetti Santos <kalebzsantosk@gmail.com>
2023-01-04 17:12:50 -0300

    update no servidor local e privado do animeflix!

--- buscarVideo.js
+++ buscarVideo.js
@@ -1,12 +1,19 @@
-import { conectaApi } from "conectaApi.js";
+import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
-    evento.preventDefalt();
-
+    evento.preventDefault();
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
-    const busca = await conectaApi.buscaVideo(dadosDePesquisa) 
+    const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-    console.log(busca);
+    const lista = document.querySelector("[data-lista]");
+
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+
+    busca.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
--- mostrarVideos.js
+++ mostrarVideos.js
@@ -1,53 +1,55 @@
-import { conectaApi } from "conectaApi.js";
+import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard(titulo, descricao, url, imagem) {
+export default function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");
     video.className = "videos__item";
-    video.innerHTML = `<div class="movie-card">
+    video.innerHTML = `<li class="video__item">
+    <div class="movie-card">
 
-    <a href="${url}">
-        <figure class="card-banner">
-            <img src="${imagem}" alt="one piece">
-        </figure>
-    </a>
-
-    <div class="title-wrapper">
         <a href="${url}">
-            <h3 class="card-title">${titulo}</h3>
+            <figure class="card-banner">
+                <img src="${imagem}" alt="">
+            </figure>
         </a>
 
-        <time datetime="1997">${descricao}</time>
-    </div>
+        <div class="title-wrapper">
+            <a href="${url}">
+                <h3 class="card-title">${titulo}</h3>
+            </a>
 
-    <div class="card-meta">
-        <div class="badge badge-outline">${descricao}</div>
-
-        <div class="duration">
-            <ion-icon name="time-outline"></ion-icon>
-
-            <time datetime="PT137M">${descricao}</time>
+            <time datetime="1997">${descricao}</time>
         </div>
 
-        <div class="rating">
-            <ion-icon name="star"></ion-icon>
+        <div class="card-meta">
+            <div class="badge badge-outline">HD</div>
 
-            <data>9.5</data>
+            <div class="duration">
+                <ion-icon name="time-outline"></ion-icon>
+
+                <time datetime="PT137M">NA</time>
+            </div>
+
+            <div class="rating">
+                <ion-icon name="star"></ion-icon>
+
+                <data></data>
+            </div>
+
         </div>
 
+
     </div>
+</li>`
 
-
-</div>
-`
     return video;
 }
 
 async function listaVideos() {
     const listaApi = await conectaApi.listaVideos();
     listaApi.forEach(elemento => lista.appendChild(
-        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
-} 
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+}
 
 listaVideos();
