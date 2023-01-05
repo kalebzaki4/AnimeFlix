22f702e0ab3967f5438adf3962420945555bc05a
Kaleb Zufanetti Santos <kalebzsantosk@gmail.com>
2023-01-05 16:28:23 -0300

    update

--- mostrarVideos.js
+++ mostrarVideos.js
@@ -1,27 +1,54 @@
-import { conectaApi } from "./conectaApi.js";
+import { conectaApi } from "./conectaApi.js"
 
-const lista = document.querySelector("[data-lista]");
+const lista = document.querySelector("[data-lista]")
 
-export default function constroiCard(titulo, descricao, url, imagem) {
-    const video = document.createElement("li");
-    video.className = "videos__item";
-    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
-    title="${titulo}" frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen></iframe>
-<div class="descricao-video">
-    <img src="${imagem}" alt="logo canal alura">
-    <h3>${titulo}</h3>
-    <p>${descricao}</p>
+function constroiCard(titulo, descricao, url, imagem) {
+    const video = document.createElement("li")
+    video.className = "videos__item"
+    video.innerHTML = `<div class="movie-card">
+
+    <a href="${url}">
+        <figure class="card-banner">
+            <img src="${imagem}" alt="one piece">
+        </figure>
+    </a>
+
+    <div class="title-wrapper">
+        <a href="${url}">
+            <h3 class="card-title">${titulo}</h3>
+        </a>
+
+        <time datetime="1997">${descricao}</time>
+    </div>
+
+    <div class="card-meta">
+        <div class="badge badge-outline">HD</div>
+
+        <div class="duration">
+            <ion-icon name="time-outline"></ion-icon>
+
+            <time datetime="PT137M">NA</time>
+        </div>
+
+        <div class="rating">
+            <ion-icon name="star"></ion-icon>
+
+            <data>9.5</data>
+        </div>
+
+    </div>
+
+
 </div>`
 
     return video;
+
 }
 
 async function listaVideos() {
-    const listaApi = await conectaApi.listaVideos();
+    const listaApi = await conectaApi.listaVideos()
     listaApi.forEach(elemento => lista.appendChild(
         constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
 
-listaVideos();
+listaVideos()
