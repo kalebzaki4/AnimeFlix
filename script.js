a460f9062b4ffd2db88d5e18e99e101cc22a8d8e
Kaleb Zufanetti Santos <kalebzsantosk@gmail.com>
2023-02-06 09:38:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const goTopBtn = document.querySelector("[data-go-top]")
 
 window.addEventListener("scroll", function () {
 
-    window.scrollY > 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active")
+    window.scrollY > 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
 
 })
 
