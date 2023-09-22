69ab98a6b4a079a4aeaaf840e830cd890fe65eb2
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-09-22 20:37:47 -0300

    Update index.jsx

--- Animeflix/src/components/Menu/index.jsx
+++ Animeflix/src/components/Menu/index.jsx
@@ -1,33 +1,30 @@
-import styles from './Menu.module.css'
-import logo from '../../assets/images/logo-animeflix.png'
+import '../../App.css'
+import LogoPesquisa from '../../assets/images/pesquisa.svg'
 
 export default function Menu() {
     return (
         <>
-            <header className={styles.header} data-header>
-                <div className={styles.container}>
+            <header className="header" data-header>
+                <div className="container">
 
-                    <div className={styles.overlay} data-overlay></div>
+                    <div className="overlay" data-overlay></div>
 
-                    <a href="index.html" className={styles.logo}>
-                        <img src={logo} alt="AnimeFlix logo" width="300px"/>
+                    <a href="index2.html" className="logo">
+                        <img src="logo-animeflix.png" alt="AnimeFlix logo" width="300px" />
                     </a>
 
                     <div className="header-actions">
 
-                        <div className="search-box">
-                            <input type="text" className="search-txt" placeholder="Pesquisar" list="historico" data-pesquisa/>
-                                <button onclick="toggle2('conteudo2')" className="search-button" data-botao-pesquisa/>
-                                    <img src="pesquisa.svg" alt="Lupa" height="20" width="20"/>
-                                <button/>
+                        <div class="search-box">
+                            <input type="text" className="search-txt" placeholder="Pesquisar" list="historico" data-pesquisa />
+                            <button onclick="toggle2('conteudo2')" className="search-button" data-botao-pesquisa />
+                            <img src="pesquisa.svg" alt="Lupa" height="20" width="20" />
                         </div>
-
-                        <a href="login.html"><button className="btn btn-primary">Entrar</button></a>
 
                     </div>
 
                     <button className="menu-open-btn" data-menu-open-btn>
-                        <ion-icon name="reorder-two"></ion-icon>
+                        <img src={LogoPesquisa} alt="" />
                     </button>
 
                     <nav className="navbar" data-navbar>
@@ -35,7 +32,7 @@ export default function Menu() {
                         <div className="navbar-top">
 
                             <a href="index.html" className="logo">
-                                <img src="logo-animeflix.png" alt="AnimeFlix logo" width="200px"/>
+                                <img src="logo-animeflix.png" alt="AnimeFlix logo" width="200px" />
                             </a>
 
                             <button className="menu-close-btn" data-menu-close-btn>
@@ -58,45 +55,13 @@ export default function Menu() {
                             </li>
 
                             <li>
-                                <a href="login.html" className="navbar-link">Entrar</a>
+                                <a href="login.html" className="navbar-link">Sobre</a>
                             </li>
+
+                            <button class="btn btn-primary">Sign in</button>
+
 
                         </ul>
-
-                        <ul className="navbar-social-list">
-
-                            <li>
-                                <a href="#" className="navbar-social-link">
-                                    <ion-icon name="logo-twitter"></ion-icon>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" className="navbar-social-link">
-                                    <ion-icon name="logo-facebook"></ion-icon>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" className="navbar-social-link">
-                                    <ion-icon name="logo-pinterest"></ion-icon>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" className="navbar-social-link">
-                                    <ion-icon name="logo-instagram"></ion-icon>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" className="navbar-social-link">
-                                    <ion-icon name="logo-youtube"></ion-icon>
-                                </a>
-                            </li>
-
-                        </ul>
-
                     </nav>
 
                 </div>
