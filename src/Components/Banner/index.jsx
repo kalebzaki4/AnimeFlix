8d32b0c34c47a4f4d8250ad9a27c6ca6e5a9e2a7
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-10-03 22:20:28 -0300

    Create index.jsx

---
+++ src/Components/Banner/index.jsx
@@ -0,0 +1,245 @@
+import "./Banner.css";
+import play from "../../assets/images/play_circle.png"
+import ola from "../../assets/images/slider-banner.jpg"
+
+export default function Banner() {
+    return (
+        <>
+            <article className="container">
+                <section className="banner" arial-label="Animes Populares">
+
+                    <div className="banner-slider">
+                        <div className="slider-item active" slider-item>
+                            <img src="https://zint.online/wp-content/uploads/2017/09/PIECE_005.jpg" alt="Banner do Animeflix" className="img-cover" loading="eager" />
+
+                            <div className="banner-content">
+                                <h2 className="heading">
+                                    One Piece
+                                </h2>
+
+                                <div className="meta-list">
+                                    <div className="meta-item">1997</div>
+
+                                    <div className="meta-item card-badge">7.5</div>
+                                </div>
+
+                                <p className="genre">Ação, Shounen, Aventura</p>
+
+                                <p className="banner-text">
+                                    Monkey D. Luffy é um jovem que sempre sonhou com uma vida de liberdade. Ele sai do vilarejo onde vive para uma jornada perigosa em busca do lendário tesouro One Piece
+                                </p>
+
+                                <a href="./detalhes" className="btn">
+                                    <img src={play} alt="Botão de Play" width={24} height={24} aria-hidden="true" />
+
+                                    <span className="span">Assistir</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="slider-item" slider-item>
+                            <img src="https://zint.online/wp-content/uploads/2017/09/PIECE_005.jpg" alt="Banner do Animeflix" className="img-cover" loading="eager" />
+
+                            <div className="banner-content">
+                                <h2 className="heading">
+                                    One Piece
+                                </h2>
+
+                                <div className="meta-list">
+                                    <div className="meta-item">1997</div>
+
+                                    <div className="meta-item card-badge">7.5</div>
+                                </div>
+
+                                <p className="genre">Ação, Shounen, Aventura</p>
+
+                                <p className="banner-text">
+                                    Monkey D. Luffy é um jovem que sempre sonhou com uma vida de liberdade. Ele sai do vilarejo onde vive para uma jornada perigosa em busca do lendário tesouro One Piece
+                                </p>
+
+                                <a href="./detalhes" className="btn">
+                                    <img src={play} alt="Botão de Play" width={24} height={24} aria-hidden="true" />
+
+                                    <span className="span">Assistir</span>
+                                </a>
+                            </div>
+                        </div> 
+
+                        <div className="slider-item" slider-item>
+                            <img src="https://zint.online/wp-content/uploads/2017/09/PIECE_005.jpg" alt="Banner do Animeflix" className="img-cover" loading="eager" />
+
+                            <div className="banner-content">
+                                <h2 className="heading">
+                                    One Piece
+                                </h2>
+
+                                <div className="meta-list">
+                                    <div className="meta-item">1997</div>
+
+                                    <div className="meta-item card-badge">7.5</div>
+                                </div>
+
+                                <p className="genre">Ação, Shounen, Aventura</p>
+
+                                <p className="banner-text">
+                                    Monkey D. Luffy é um jovem que sempre sonhou com uma vida de liberdade. Ele sai do vilarejo onde vive para uma jornada perigosa em busca do lendário tesouro One Piece
+                                </p>
+
+                                <a href="./detalhes" className="btn">
+                                    <img src={play} alt="Botão de Play" width={24} height={24} aria-hidden="true" />
+
+                                    <span className="span">Assistir</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="slider-item" slider-item>
+                            <img src="https://zint.online/wp-content/uploads/2017/09/PIECE_005.jpg" alt="Banner do Animeflix" className="img-cover" loading="eager" />
+
+                            <div className="banner-content">
+                                <h2 className="heading">
+                                    One Piece
+                                </h2>
+
+                                <div className="meta-list">
+                                    <div className="meta-item">1997</div>
+
+                                    <div className="meta-item card-badge">7.5</div>
+                                </div>
+
+                                <p className="genre">Ação, Shounen, Aventura</p>
+
+                                <p className="banner-text">
+                                    Monkey D. Luffy é um jovem que sempre sonhou com uma vida de liberdade. Ele sai do vilarejo onde vive para uma jornada perigosa em busca do lendário tesouro One Piece
+                                </p>
+
+                                <a href="./detalhes" className="btn">
+                                    <img src={play} alt="Botão de Play" width={24} height={24} aria-hidden="true" />
+
+                                    <span className="span">Assistir</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="slider-item" slider-item>
+                            <img src="https://zint.online/wp-content/uploads/2017/09/PIECE_005.jpg" alt="Banner do Animeflix" className="img-cover" loading="eager" />
+
+                            <div className="banner-content">
+                                <h2 className="heading">
+                                    One Piece
+                                </h2>
+
+                                <div className="meta-list">
+                                    <div className="meta-item">1997</div>
+
+                                    <div className="meta-item card-badge">7.5</div>
+                                </div>
+
+                                <p className="genre">Ação, Shounen, Aventura</p>
+
+                                <p className="banner-text">
+                                    Monkey D. Luffy é um jovem que sempre sonhou com uma vida de liberdade. Ele sai do vilarejo onde vive para uma jornada perigosa em busca do lendário tesouro One Piece
+                                </p>
+
+                                <a href="./detalhes" className="btn">
+                                    <img src={play} alt="Botão de Play" width={24} height={24} aria-hidden="true" />
+
+                                    <span className="span">Assistir</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="slider-item" slider-item>
+                            <img src="https://zint.online/wp-content/uploads/2017/09/PIECE_005.jpg" alt="Banner do Animeflix" className="img-cover" loading="eager" />
+
+                            <div className="banner-content">
+                                <h2 className="heading">
+                                    One Piece
+                                </h2>
+
+                                <div className="meta-list">
+                                    <div className="meta-item">1997</div>
+
+                                    <div className="meta-item card-badge">7.5</div>
+                                </div>
+
+                                <p className="genre">Ação, Shounen, Aventura</p>
+
+                                <p className="banner-text">
+                                    Monkey D. Luffy é um jovem que sempre sonhou com uma vida de liberdade. Ele sai do vilarejo onde vive para uma jornada perigosa em busca do lendário tesouro One Piece
+                                </p>
+
+                                <a href="./detalhes" className="btn">
+                                    <img src={play} alt="Botão de Play" width={24} height={24} aria-hidden="true" />
+
+                                    <span className="span">Assistir</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="slider-item" slider-item>
+                            <img src="https://zint.online/wp-content/uploads/2017/09/PIECE_005.jpg" alt="Banner do Animeflix" className="img-cover" loading="eager" />
+
+                            <div className="banner-content">
+                                <h2 className="heading">
+                                    One Piece
+                                </h2>
+
+                                <div className="meta-list">
+                                    <div className="meta-item">1997</div>
+
+                                    <div className="meta-item card-badge">7.5</div>
+                                </div>
+
+                                <p className="genre">Ação, Shounen, Aventura</p>
+
+                                <p className="banner-text">
+                                    Monkey D. Luffy é um jovem que sempre sonhou com uma vida de liberdade. Ele sai do vilarejo onde vive para uma jornada perigosa em busca do lendário tesouro One Piece
+                                </p>
+
+                                <a href="./detalhes" className="btn">
+                                    <img src={play} alt="Botão de Play" width={24} height={24} aria-hidden="true" />
+
+                                    <span className="span">Assistir</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slider-control">
+                        <div className="control-inner">
+                            <button className="poster-box slider-item active">
+                                <img src={ola} alt="" loading="lazy" draggable="false"/>
+                            </button>
+
+                            <button className="poster-box slider-item active">
+                                <img src={ola} alt="" loading="lazy" draggable="false"/>
+                            </button>
+
+                            <button className="poster-box slider-item active">
+                                <img src={ola} alt="" loading="lazy" draggable="false"/>
+                            </button>
+
+                            <button className="poster-box slider-item active">
+                                <img src={ola} alt="" loading="lazy" draggable="false"/>
+                            </button>
+
+                            <button className="poster-box slider-item active">
+                                <img src={ola} alt="" loading="lazy" draggable="false"/>
+                            </button>
+
+                            <button className="poster-box slider-item active">
+                                <img src={ola} alt="" loading="lazy" draggable="false"/>
+                            </button>
+
+                            <button className="poster-box slider-item active">
+                                <img src={ola} alt="" loading="lazy" draggable="false"/>
+                            </button>
+                        </div>
+                    </div>
+
+                </section>
+            </article>
+        </>
+    )
+}
