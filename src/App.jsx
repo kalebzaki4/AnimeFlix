d9595bea0cc9167f76b93cd7fb5bc2185d0f3ad7
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-10-03 22:20:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Banner from './Components/Banner'
 import ListaLateral from './Components/ListaLateral'
 import Menu from './Components/Menu'
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Menu />
       <ListaLateral />
+      <Banner />
     </>
   )
 }
