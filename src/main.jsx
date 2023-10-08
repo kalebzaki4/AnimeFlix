9d090c3296142fac2c75a4dad99e548604a95604
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-10-08 16:58:52 -0300

    Delete main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Routes from './Routes.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-)
