a44208552fa024aaaa762095eac0bb924bf24fbf
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-10-19 10:38:40 -0300

    PROJETO COMPLETO 19/10/2023

---
+++ src/Paginas/Inicio/index.jsx
@@ -0,0 +1,14 @@
+import Banner from "../../Components/Banner";
+import Animes from "../../Components/ListaDeAnimes";
+import ListaLateral from "../../Components/ListaLateral";
+import Menu from "../../Components/Menu";
+
+export default function Inicio() {
+    return (
+        <>
+            <Banner />
+            <ListaLateral/>
+            <Animes/>
+        </>
+    )
+}
