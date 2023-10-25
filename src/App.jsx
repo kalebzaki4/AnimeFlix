973fd5e89aa71efe10e21e9cc93e7f8ceb1ca38a
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-10-25 11:10:17 -0300

    projeto em andamento de lista 25/10/23

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Menu from './Components/Menu';
+import Inicio from './Paginas/Inicio';
+import PaginaDetalhes from './Paginas/PaginaDetalhes';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route element={<Inicio />} path='/' />
+          <Route element={<PaginaDetalhes />} path='/Detalhes/:id' />
+          <Route element={<h1>Página Não Encontrada</h1>} path='*' />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
