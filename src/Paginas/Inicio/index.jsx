4942c272fb60a7baed95fd3d0c94751c94ce5804
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-12-26 18:51:07 -0300

    Create index.jsx

---
+++ src/Paginas/Inicio/index.jsx
@@ -0,0 +1,13 @@
+import Banner from "../../Components/Banner";
+import Animes from "../../Components/ListaDeAnimesHorizontal";
+import ListaLateral from "../../Components/ListaLateral";
+
+export default function Inicio() {
+    return (
+        <>
+            <Banner />
+            <ListaLateral/>
+            <Animes/>
+        </>
+    )
+}
