f2509bb42fb27101eae6621572f5dbf059dcfda9
kalebzaki4 <kalebzsantosk@gmail.com>
2024-07-03 17:41:26 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,20 @@ import './App.css';
 import Menu from './Components/Menu';
 import Inicio from './Paginas/Inicio';
 import PaginaDetalhes from './Paginas/PaginaDetalhes';
-  import Erro404 from './Components/Erro404';
+import Erro404 from './Components/Erro404';
 import ResultadoAnimes from './Paginas/ResultadoAnimes';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Menu />
-        <Routes>
-          <Route element={<Inicio />} path='/' />
-          <Route element={<PaginaDetalhes />} path='/Detalhes/:id' />
-          <Route element={<ResultadoAnimes />} path='/a' />
-          <Route element={<Erro404 />} path='*' />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path='/Detalhes/:id' element={<PaginaDetalhes />} />
+        <Route path='/a' element={<ResultadoAnimes />} />
+        <Route path='*' element={<Erro404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
