711cc083fa5f7c91489cbd780ddb5965161e330d
kalebzaki4 <kalebzsantosk@gmail.com>
2024-07-04 15:37:28 -0300

    Update index.jsx

--- src/Paginas/Inicio/index.jsx
+++ src/Paginas/Inicio/index.jsx
@@ -1,12 +1,10 @@
 import Banner from "../../Components/Banner";
 import Animes from "../../Components/ListaDeAnimesHorizontal";
-import ListaLateral from "../../Components/ListaLateral";
 
 export default function Inicio() {
     return (
         <>
             <Banner />
-            <ListaLateral/>
             <Animes/>
         </>
     )
