93d81fdc5a5af3b11ef2e344b7a52df5a18a4b1b
kalebzaki4 <kalebzsantosk@gmail.com>
2024-07-07 19:54:57 -0300

    Create index.jsx

---
+++ src/Paginas/Inicio/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../../Components/Banner/index";
+import ListaDeAnimesHorizontal from "../../Components/ListaDeAnimesHorizontal/index";
+
+export default function Inicio() {
+    return (
+        <>
+            <Banner />
+            <ListaDeAnimesHorizontal />
+        </>
+    );
+}
