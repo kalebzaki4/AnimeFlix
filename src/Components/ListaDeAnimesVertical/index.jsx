bce267168166cc03a937d731f85bb33473479545
kalebzaki4 <kalebzsantosk@gmail.com>
2024-07-07 19:53:21 -0300

    Create index.jsx

---
+++ src/Components/ListaDeAnimesVertical/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Estrelas from '../../assets/images/star.png';
+import data from '../../data';
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
+function ListaAnimesVertical() {
+    const query = useQuery();
+    const searchTerm = query.get('q');
+
+    const filteredData = data.filter(anime =>
+        anime.titulo.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <article className="container-inative-2">
+            <section className="movie-list" aria-label="upcoming movie">
+                <div className="title-wrapper">
+                    <h2 className="title-large">Resultados da Pesquisa</h2>
+                </div>
+                <div className="grid-list">
+                    {filteredData.map((anime) => (
+                        <div className="movie-card" key={anime.id}>
+                            <figure className="poster-box card-banner">
+                                <img src={anime.imagem} alt={anime.titulo} className="img-cover" />
+                            </figure>
+                            <h4 className="title">{anime.titulo}</h4>
+                            <div className="meta-list">
+                                <div className="meta-item">
+                                    <img src={Estrelas} width={20} height={20} loading="lazy" alt="Avaliação" />
+                                    <span className="span">{anime.avaliacao}</span>
+                                </div>
+                                <div className="card-badge">{anime.ano}</div>
+                            </div>
+                            <Link to={`/Detalhes/${anime.id}`} className="card-btn" title="Detalhes"></Link>
+                        </div>
+                    ))}
+                </div>
+                <button className="btn load-more">Ver Mais</button>
+            </section>
+        </article>
+    );
+}
+
+export default ListaAnimesVertical;
