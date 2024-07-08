7c806d22cf1d60c44d8a459f72629bf8828e40a4
kalebzaki4 <kalebzsantosk@gmail.com>
2024-07-08 11:07:24 -0300

    Update index.jsx

--- src/Paginas/Inicio/index.jsx
+++ src/Paginas/Inicio/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Banner from "../../Components/Banner";
-import ListaDeAnimesHorizontal from "../../Components/ListaDeAnimesHorizontal";
+import Banner from "../../Components/Banner/index";
+import ListaDeAnimesHorizontal from "../../Components/ListaDeAnimesHorizontal/index";
 
 export default function Inicio() {
     return (
