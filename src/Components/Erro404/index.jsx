b18c9c4cd481741589cd8cac3c0568c78fc2c7dc
kalebzaki4 <kalebzsantosk@gmail.com>
2024-07-23 11:57:40 -0300

    Update index.jsx

--- src/Components/Erro404/index.jsx
+++ src/Components/Erro404/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Erro404.css"
+import ImgErro from "../../assets/images/Erro.png"
 
 const Erro404 = () => {
     return (
@@ -7,7 +8,7 @@ const Erro404 = () => {
             <h1 className="error-title">Erro 404 - Página não encontrada</h1>
             <p className="error-message">Desculpe, a página que você está procurando não existe.</p>
             <p className="error-message">Volte para a <a href="/" className="error-link">página inicial</a> do nosso site de animes.</p>
-            <img src="https://animetvonline.cx/wp-content/themes/animestream_new/assets/images/404.png" alt="GIF de erro" width={200} className="error-gif" />
+            <img src={ImgErro} alt="IMG de erro" width={150} className="error-img" />
         </div>
     );
 }
