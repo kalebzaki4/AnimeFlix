7e191e1ff5a2db6e45b7dcc400f0e10c34aa9ba7
kalebzaki4 <kalebzsantosk@gmail.com>
2024-08-12 18:16:35 -0300

    Update index.jsx

--- src/Paginas/Inicio/index.jsx
+++ src/Paginas/Inicio/index.jsx
@@ -3,11 +3,17 @@ import Banner from "../../Components/Banner/index";
 import ListaDeAnimesHorizontal from "../../Components/ListaDeAnimesHorizontal/index";
 
 export default function Inicio() {
-    return (
-        <>
-            <Banner />
-            <ListaDeAnimesHorizontal />
-            <ListaDeAnimesHorizontal />
-        </>
-    );
+  return (
+    <>
+      <Banner />  
+      <ListaDeAnimesHorizontal 
+        title="Mais Famosos" 
+        description="Fique por dentro de todos os lançamentos"
+      />
+      <ListaDeAnimesHorizontal 
+        title="Lançamentos" 
+        description="Veja os animes mais recentes"
+      />
+    </>
+  );
 }
