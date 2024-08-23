0d0349cda206e95f7ac57a644edef71487f0b144
kalebzaki4 <kalebzsantosk@gmail.com>
2024-08-23 18:00:34 -0300

    Update index.jsx

--- src/Components/TelaCarregamento/index.jsx
+++ src/Components/TelaCarregamento/index.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './TelaCarregamento.css';
 
-export default function LoadingScreen() {
+export default function TelaCarregamento() {
+  const [mostrarCarregamento, setMostrarCarregamento] = useState(true);
+
+  useEffect(() => {
+    const temporizador = setTimeout(() => {
+      setMostrarCarregamento(false);
+    }, 500); // Tempo de exibição reduzido para 500ms
+
+    return () => clearTimeout(temporizador); // Limpa o temporizador quando o componente é desmontado
+  }, []);
+
+  if (!mostrarCarregamento) {
+    return null; // Não renderiza nada se o carregamento não deve ser mostrado
+  }
+
   return (
-    <div className="loading-screen">
-      <div className="spinner"></div>
+    <div className="tela-carregamento">
+      <div className="carregando"></div>
     </div>
   );
 }
