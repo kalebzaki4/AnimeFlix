06bc6a5e813d3ee959fc253129210cb6bcaaee8f
kalebzaki4 <kalebzsantosk@gmail.com>
2024-08-28 19:14:14 -0300

    Update index.jsx

--- src/Paginas/ResultadoAnimes/index.jsx
+++ src/Paginas/ResultadoAnimes/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Estrelas from '../../assets/images/star.png';
 import './ListaAnimesVertical.css';
-import data from "../../data";
-
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
