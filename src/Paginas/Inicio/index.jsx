34f09ad4fa0fd043027c0e24240dcd7fe534efff
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-01-22 11:10:12 -0300

    Update index.jsx

--- src/Paginas/Inicio/index.jsx
+++ src/Paginas/Inicio/index.jsx
@@ -8,12 +8,16 @@ import '../../message.css';
 export default function Inicio() {
   const [loading, setLoading] = useState(true);
   const [animes, setAnimes] = useState([]);
+  const [lancamentos, setLancamentos] = useState([]);
 
   useEffect(() => {
     const fetchAnimes = async () => {
       try {
         const response = await axios.get("https://api.jikan.moe/v4/anime");
-        setAnimes(response.data.data);
+        const sortedAnimes = response.data.data.sort((a, b) => new Date(b.start_date) - new Date(a.start_date));
+        const latestReleases = sortedAnimes.slice(0, 10);
+        setAnimes(sortedAnimes);
+        setLancamentos(latestReleases);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching anime data:", error);
@@ -38,8 +42,8 @@ export default function Inicio() {
       <ListaDeAnimesHorizontal 
         title="Lançamentos" 
         description="Veja os animes mais recentes"
-        animes={animes}
+        animes={lancamentos}
       />
     </>
   );
-}
+};
