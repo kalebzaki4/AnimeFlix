23e3eecd903505b97e0b0758274d823c7faed616
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-08-10 12:18:52 -0300

    Update index.jsx

--- src/pages/AZ/index.jsx
+++ src/pages/AZ/index.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useCallback, useRef } from "react";
 import { apiGet } from "../../utils/api";
 import "./AZ.scss";
 import { Link, useNavigate } from "react-router-dom";
-import Estrelas from "../../components/animes/Estrelas/Estrelas";
 import { fetchMissingImages } from "../../utils/fetchUtils";
 
 const LIMIT = 18;
